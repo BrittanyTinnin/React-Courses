809bed40e24bc8b67d16e224d0cf1b7249fc87e3
github username <tinnin.brittany.n@gmail.com>
2020-04-13 19:05:16 -0400

    using state to reveal location using conditionally statements

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -11,15 +11,45 @@ class App extends React.Component {
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
         //we called setState to update state object
-        this.setState({ lat: position.coords.latitude });
+        console.log(position);
+        this.setState({ lat: position.coords.latitude, errorMessage: "" });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errorMessage: err.message });
+        //not required to update every property of state
+      }
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+/*
+Timeline Execution of above code
+- JS file loaded by browswer
+- Instance of App component is created
+- App components 'constructor' function gets called
+- State object is created and assigned to the 'this.state' property
+- We call geolocation service
+- React calls the components render method
+-App returns JSX, gets rendered to page as HTML
+...
+- we get result of geolcation
+- We update our state object with a call to 'this.setState'
+- React sees that we updated the state of a component
+- React calls our 'render' method a second time
+- Render method returns some (updated) JSX
+- React takes that JSX and updates content on the screen
+*/
