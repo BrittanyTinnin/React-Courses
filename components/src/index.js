2c6fc477f6913f2c5eba6f3bb2dcbf075726d0e4
github username <tinnin.brittany.n@gmail.com>
2020-04-13 16:03:09 -0400

    update

--- components/src/index.js
+++ components/src/index.js
@@ -1,15 +1,28 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
 
-import faker from "faker";
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam"/>
-      <CommentDetail author="Alex"/>
-      <CommentDetail author="Jane"/>
+      <CommentDetail
+        avatar={faker.image.avatar}
+        author="Sam"
+        timeAgo="Today at 4:45PM"
+        text="This is a sentence."
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo={}
+        text={faker.lorem.sentence}
+      />
+      <CommentDetail
+        author="Jane"
+        timeAgo="Today at 4:45PM"
+        text={faker.lorem.sentence}
+      />
     </div>
   );
 };
