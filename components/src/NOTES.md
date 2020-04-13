66f35701b042ccabe20a23cbd9e5d9009a9dd98e
github username <tinnin.brittany.n@gmail.com>
2020-04-13 17:13:56 -0400

    added to notes

--- components/src/NOTES.md
+++ components/src/NOTES.md
@@ -1,4 +1,5 @@
 ## Steps to Duplicate An Component
+
 1. Identiy the JSX that appears to be duplicated
 2. What is the purpose of that block of JSX? Think of a descriptive name for what it does
 3. Create a new file to house this new component - it should have the same name as the component
@@ -6,8 +7,26 @@
 5. Make the new componenet configurable by using React's 'props' system
 
 ## Props
+
 - System for passing data from a parent component to a child component
 - Goal is to customize or configure a child component
 - unidirectional
 - props stands for properties
 - ex: <CommentDetail author="Sam" />, author is name of prop, Sam is value of prop
+- another ex:
+  <ApprovalCard>
+        <CommentDetail
+            avatar={faker.image.avatar()}
+            author={faker.name.firstName()}
+            timeAgo="Today at 4:45PM"
+            content={faker.lorem.sentences()}
+          />
+  </ApprovalCard>
+
+  ## Components
+  - Functional Components: Good for simple content
+  - Class Components: Good for just about everything else
+  - Class Components:
+    - Easier code organization
+    - Can use 'state' (another React system) -> easier to handle user input
+    - Understands lifecycle events -> easier to do things when the app first starts
--- components/src/index.js
+++ components/src/index.js
@@ -1,30 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="Today at 4:45PM"
-        content={faker.lorem.sentences()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="Today at 2:00AM"
-        content={faker.lorem.sentences()}
-      />
-      <CommentDetail
-        avatar={faker.image.avatar()}
-        author={faker.name.firstName()}
-        timeAgo="Today at 4:45PM"
-        content={faker.lorem.sentences()}
-      />
+      <ApprovalCard>
+        <h4>Warning!</h4>
+        Are you sure you want to do this?
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="Today at 2:00AM"
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          avatar={faker.image.avatar()}
+          author={faker.name.firstName()}
+          timeAgo="Today at 4:45PM"
+          content={faker.lorem.sentences()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
