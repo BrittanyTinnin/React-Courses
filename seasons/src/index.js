6f18d754ee0b1b245743d38f04bbb1ff94529b8f
github username <tinnin.brittany.n@gmail.com>
2020-04-13 18:03:05 -0400

    defined constructor and initialized state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props); //super calls the constructor function inside React.Component, calls parent constructor
+    //initialize state here
+
+    this.state = { lat: null };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
