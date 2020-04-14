e93379cddbd78f6ab940096d92de8416e3ab7a79
github username <tinnin.brittany.n@gmail.com>
2020-04-14 10:41:13 -0400

    implemented axios http requester

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-import axios from 'axios';
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
   onSerachSubmit(term) {
-    console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID ZtOl_qhmUpn376dLX5G5jrjN3PD3avlVCxzh8ygxu4c",
+      },
+    });
   }
 
   render() {
