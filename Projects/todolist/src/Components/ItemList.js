681ad4405829dc5a7166503557279aa2e9579f51
github username <tinnin.brittany.n@gmail.com>
2020-04-17 11:33:55 -0400

    implemented another component

--- Projects/todolist/src/Components/ItemList.js
+++ Projects/todolist/src/Components/ItemList.js
@@ -3,12 +3,13 @@ import Item from "./Item";
 
 class ItemList extends React.PureComponent {
   render() {
-    console.log("array of items" + this.props.items);
-    console.log(this.props.items);
-
     const itemList = this.props.items.map((item) => <Item item={item} />);
 
-    return <div>{itemList}</div>;
+    return (
+      <div>
+        <Item item={itemList} />
+      </div>
+    );
   }
 }
 
