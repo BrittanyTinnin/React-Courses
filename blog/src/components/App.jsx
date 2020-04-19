48c00aef8f77ec713866cf8a2b422c589eca868f
github username <tinnin.brittany.n@gmail.com>
2020-04-19 10:05:51 -0400

    setup app

---
+++ blog/src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const App = () => {
+  // return <div className="ui container">App!</div>;
+  return <div>App!</div>;
+};
+
+export default App;
---
+++ blog/src/reducers/index.jsx
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  dummy: () => "hi there", //no purpose just tricking redux so app will work until setup is complete
+});
