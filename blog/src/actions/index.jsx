6e1e899b09e7f66b7480d70a87df3f5dcb85714c
github username <tinnin.brittany.n@gmail.com>
2020-04-19 21:17:10 -0400

    mid app update

--- blog/src/actions/index.jsx
+++ blog/src/actions/index.jsx
@@ -6,7 +6,9 @@ export const fetchPosts = () => {
   //defined a function that's going to return a function
   return async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts");
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data }); //dispatch function initiate changes to data the on redux side of app
+
+    // action creator returns a function, redux-thunk sees that its a function, and invokes function with dispatch argument. once the function is done, we call dispatch manually to dispatch an action, if it returns an object then its dispatched to reducers
   };
 
   // return {
@@ -16,3 +18,10 @@ export const fetchPosts = () => {
 };
 
 // - action creators must return plain js objects with a type property
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  };
+};
--- blog/src/components/PostList.jsx
+++ blog/src/components/PostList.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
+import UserHeader from "./UserHeader";
 
 class PostList extends React.PureComponent {
   componentDidMount() {
     this.props.fetchPosts(); //calls action creator
   }
 
+  renderList() {
+    //helperMethod
+    return this.props.posts.map((post) => {
+      return (
+        <div className="item" key={post.id}>
+          <i className="large middle aligned icon user" />
+          <div className="content">
+            <div className="description">
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+            <UserHeader userId={post.userId} />
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>Post List!</div>;
+    return <div className="ui relaxed divided list">{this.renderList()}</div>;
   }
 }
 
-export default connect(null, { fetchPosts: fetchPosts })(PostList); //arguments of connect method -> mapStateToProps, mapDispatchToProps or object for action creators -> passing an object instead of a function means connect with automatically call bindActionCreators for you internally
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+};
+
+export default connect(mapStateToProps, { fetchPosts: fetchPosts })(PostList); //arguments of connect method -> mapStateToProps, mapDispatchToProps or object for action creators -> passing an object instead of a function means connect with automatically call bindActionCreators for you internally
