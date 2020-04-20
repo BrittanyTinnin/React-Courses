f59ca5fddc820f075efe3c0f101d74f9ea073e0a
github username <tinnin.brittany.n@gmail.com>
2020-04-20 19:51:29 -0400

    setup app

--- blogv2/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
-
-//implement redux, react-redux, redux, axios, redux-thunk
