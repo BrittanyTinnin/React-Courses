b20cb7e21e5e6efe4e0e06b01048dec2dd6a2438
github username <tinnin.brittany.n@gmail.com>
2020-04-20 21:08:55 -0400

    testing out components and added semantic ui cdn

--- blogv2/src/components/App.jsx
+++ blogv2/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import PostList from "./PostList";
+import UserHeader from "./UserHeader";
 
 const App = () => {
-  return <div>App!</div>;
+  return (
+    <div>
+      App!
+      <PostList />
+      <UserHeader />
+    </div>
+  );
 };
 
 export default App;
