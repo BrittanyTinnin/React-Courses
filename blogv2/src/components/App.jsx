26f4ee1dc8329f9bd76475f1fa8cb7d6d256ba19
github username <tinnin.brittany.n@gmail.com>
2020-04-22 19:36:10 -0400

    update

--- blogv2/src/components/App.jsx
+++ blogv2/src/components/App.jsx
@@ -1,13 +1,10 @@
 import React from "react";
 import PostList from "./PostList";
-import UserHeader from "./UserHeader";
 
 const App = () => {
   return (
     <div>
-      App!
       <PostList />
-      <UserHeader />
     </div>
   );
 };
--- blogv2/src/components/PostList.jsx
+++ blogv2/src/components/PostList.jsx
@@ -7,3 +7,11 @@ class PostList extends React.Component {
 }
 
 export default PostList;
+
+// STEPS:
+// 1. show a list of posts in the PostList Component
+//   - connect the app to the redux store
+//   - componentDidMount the lifecycle method should call an action creator
+//   - setup action creator, use axios to make the call
+//   - setup reducer
+//   - have the blog posts show up in the class component PostList
