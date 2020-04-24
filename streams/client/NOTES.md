8d45bd82c74aec65b82662d05d876e20650455f1
github username <tinnin.brittany.n@gmail.com>
2020-04-23 23:53:51 -0400

    implement google oauth

--- streams/client/NOTES.md
+++ streams/client/NOTES.md
@@ -9,3 +9,11 @@
 ## Uses
 
 - use _Link_ component in replace of any anchor tags
+
+## Steps for Setting Up OAuth
+
+- Create a new project at console.developers.google.com/
+- Set up an OAuth confirmation screen
+- Generate an OAuth Client ID
+- Install Google's API library, initialize it with the OAuth Client ID
+- Make sure the lib gets called any time the user clicks on the 'Login with Google' button
---
+++ streams/client/src/components/GoogleAuth.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "773340402995-qrh98dd09r5gt1encm2ovb1hij9l9au5.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
+  }
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
