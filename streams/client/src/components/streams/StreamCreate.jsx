9f3ca13475ed2decd26b9a53b6b58f425f27ab57
github username <tinnin.brittany.n@gmail.com>
2020-04-24 15:59:23 -0400

    implementing redux form

--- streams/client/src/components/streams/StreamCreate.jsx
+++ streams/client/src/components/streams/StreamCreate.jsx
@@ -1,7 +1,45 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate!</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    console.log(formProps);
+    return (
+      <div className="field">
+        <label>{formProps.label}</label>
+        <input {...formProps.input} />
+      </div>
+      // <input
+      //   onChange={formProps.input.onChange}
+      //   value={formProps.input.value}
+      // />
+    );
+  }
 
-export default StreamCreate;
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
+
+//Field component doesn't render anything. it there to hook up the infastructure of reduxForm
