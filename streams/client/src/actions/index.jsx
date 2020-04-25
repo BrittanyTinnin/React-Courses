b10bb6ed8cccf26ff2c4cdeb29de81bf630ebf4f
github username <tinnin.brittany.n@gmail.com>
2020-04-24 21:45:30 -0400

    update

--- streams/client/src/actions/index.jsx
+++ streams/client/src/actions/index.jsx
@@ -1,4 +1,13 @@
-import { SIGN_IN, SIGN_OUT } from "./types";
+import streams from "../apis/streams";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+} from "./types";
 
 export const signIn = (userId) => {
   return { type: SIGN_IN, payload: userId };
@@ -6,4 +15,41 @@ export const signIn = (userId) => {
 
 export const signOut = () => {
   return { type: SIGN_OUT };
+};
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get("/streams");
+
+    dispatch({ type: FETCH_STREAMS, action: response.data });
+  };
+};
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({ type: FETCH_STREAM, action: response.data });
+  };
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await streams.post("/streams", formValues);
+
+    dispatch({ type: CREATE_STREAM, action: response.data });
+  };
+};
+
+export const editRecord = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({ type: EDIT_STREAM, action: response.data });
+  };
+};
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+    dispatch({ type: DELETE_STREAM, payload: id });
+  };
 };
