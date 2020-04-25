aeb2c5989b5d4e7a124d9901650c01fa1cd29342
github username <tinnin.brittany.n@gmail.com>
2020-04-25 13:05:01 -0400

    creating stream reducer

---
+++ streams/client/src/reducers/streamReducer.jsx
@@ -0,0 +1,39 @@
+// import { EDIT_STREAM } from "../actions/types";
+
+// const streamReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case EDIT_STREAM:
+//       const newState = { ...state };
+//       newState[action.payload.id] = action.payload;
+//       return newState;
+//     // return { ...state, [action.payload.id]: action.payload };
+//     default:
+//       return state;
+//   }
+// };
+
+import {
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      console.log(action);
+      return;
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return { [action.payload.id]: action.payload, ...state };
+    default:
+      return state;
+  }
+};
