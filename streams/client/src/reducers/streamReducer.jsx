16c2e2bf4c2767eaf37c28d249c64a3cb4175170
github username <tinnin.brittany.n@gmail.com>
2020-07-07 12:20:52 -0700

    debugging

--- streams/client/src/reducers/streamReducer.jsx
+++ streams/client/src/reducers/streamReducer.jsx
@@ -23,6 +23,7 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
+      console.log("in reducer, state-keys: " + Object.keys(state));
       let newObject = {};
       action.payload.forEach((stream) => (newObject[stream.id] = stream));
       return { ...state, ...newObject };
@@ -31,10 +32,21 @@ export default (state = {}, action) => {
     case CREATE_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case EDIT_STREAM:
-      console.log(action.payload.id);
       return { ...state, [action.payload.id]: action.payload };
     case DELETE_STREAM:
+      // console.log("state-keys: " + Object.keys(state));
+      // console.log("action.payload: " + action.payload);
+      // let obj = Object.assign({}, state);
+      // { ...state, items: state.items.filter(i => i.id !== action.payload.id };
+      // const doSomething = (obj, prop) => {
+      //   let res = Object.assign({}, obj)
+      //   delete res[prop]
+      //   return res
+      // }
+      // delete obj[action.payload];
+      // return { ...state, obj };
       return { ...state, [action.payload.id]: action.payload };
+
     default:
       return state;
   }
