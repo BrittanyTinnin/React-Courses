39eab10c10549185d835cc7120c82c2ebc4cf3bc
github username <tinnin.brittany.n@gmail.com>
2020-07-20 11:13:00 -0700

    connected front to backend

---
+++ frontend-spring-boot-react-crud-full-stack-with-maven/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import InstructorApp from "./components/InstructorApp";
+
+const App = () => {
+  return (
+    <div className="container">
+      <InstructorApp />
+    </div>
+  );
+};
+
+export default App;
