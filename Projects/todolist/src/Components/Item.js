95a781c7059ee81e025d0a8d2d249cd5ce1e031b
github username <tinnin.brittany.n@gmail.com>
2020-09-26 15:29:40 -0700

    update

--- Projects/todolist/src/Components/Item.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Item = (props) => {
-  if (props.item) {
-    return <div className="content">{props.item}</div>;
-  }
-};
-
-export default Item;
--- blogv2/src/reducers/postsReducer.jsx
+++
@@ -1,9 +0,0 @@
-export default function postsReducer(state = [], action) {
-  switch (action.type) {
-    case "FETCH_POSTS":
-      console.log(action.payload);
-      return action.payload;
-    default:
-      return state;
-  }
-}
